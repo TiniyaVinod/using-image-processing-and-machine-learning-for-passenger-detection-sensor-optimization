848dec1043532ff0e4f29f1baab5c886809f031c
paweenp <paween.pongsomboon@gmail.com>
2022-02-03 13:58:28 +0100

    test with new dataset

--- Program/Matlab/program.m
+++ Program/Matlab/program.m
@@ -1,39 +1,61 @@
 % Program to detect a human from top-view basing on Background subtraction
 % (Fixed environment)
-% 1.) Source selection (video or webcam)
-% 2.) Import background image (fixed background)
-% 3.) Acquire frames from source
-% 4.) Process the frame
-% 5.) Show the result
-
-%% 1.) Source selection
-% Now support only video
-videoSource = VideoReader('human_sit.mp4');
+% 0.) Parameter Setting
+% 1.) Source Selection (video or webcam)
+% 2.) Background subtraction
 
 
+
+%% 0.) Parameter Setting
+data_path = "..\..\Data\Videos";
+data_filetype = ".png";
+frame_asp_ratio = [128 128];
+
+ref_img = imread('ref.jpg');
+ref_img = imresize(ref_img, frame_asp_ratio);
+ref_img = rgb2gray(ref_img);
+
+
+video_path = 'human_sit-4.mp4';
 
 
 detector = vision.ForegroundDetector(...
        'NumTrainingFrames', 20, ...
-       'InitialVariance', 100*200);
+       'InitialVariance', 30*30);
+   
    
 blob = vision.BlobAnalysis(...
        'CentroidOutputPort', false, 'AreaOutputPort', false, ...
        'BoundingBoxOutputPort', true, ...
-       'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 250);
-
+       'MinimumBlobAreaSource', 'Property', 'MinimumBlobArea', 1000);   
+   
 shapeInserter = vision.ShapeInserter('BorderColor','White');
+   
+v = VideoReader(video_path);
 
-videoPlayer = vision.VideoPlayer();
+prev_frame = [];
 
-while hasFrame(videoSource)
-     frame  = readFrame(videoSource);
-     frame = imresize(frame, 0.1);
-     fgMask = detector(frame);
-     bbox   = blob(fgMask);
-     out    = shapeInserter(frame,bbox);
-     videoPlayer(out);
-     pause(0.1);
+while hasFrame(v)
+    
+    frameRGB = readFrame(v);
+    frameRGB = imresize(frameRGB, frame_asp_ratio);
+    frameGray = rgb2gray(frameRGB);
+
+    out = imabsdiff(frameGray, ref_img);
+    subplot 221, imshow(out), title('imabsdiff');
+    
+    t = graythresh(out);
+    out_t = imbinarize(out, 0.35);
+    subplot 222, imshow(out_t), title('imbinarize');
+    
+%     if ~isempty(prev_frame)
+%         out = imabsdiff(frameGray, prev_frame);
+%         imshow(out);
+%     end
+    
+    fgMask = detector(out);    
+    bbox   = blob(fgMask);
+    out_detector   = shapeInserter(frameGray,bbox);
+    subplot 223, imshow(out_detector), title('foreground_detector');
+    drawnow;
 end
-
-release(videoPlayer);
