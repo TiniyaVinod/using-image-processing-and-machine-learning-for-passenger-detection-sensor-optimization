8966097a88d7beb0b5abe5ca1c3dbfb7b12ee1cd
paweenp <paween.pongsomboon@gmail.com>
2022-05-03 11:21:11 +0200

    program works properly

--- Program/python/person-detection/person-detection/person_detection.py
+++ Program/python/person-detection/person-detection/person_detection.py
@@ -4,90 +4,98 @@
 import tkinter as tk
 import cv2
 import numpy as np
-import PIL.Image, PIL.ImageTk
-import time
+from PIL import Image, ImageTk
 from LiveVideoCapture import LiveVideoCapture
 from detect_main import FaceDetection
 
-class main_program:
-    def __init__(self, window, window_title, face_cascade, video_source=0):
+def play():
+    '''
+    start stream (run_camera and update_image) 
+    and change state of buttons
+    '''
+    global run_camera
+
+    if not run_camera:
+        run_camera = True
         
-        # main window for application
-        self.window = window
-        self.window.title(window_title)
-        self.video_source = video_source
-
-        # store face cascade model
-        self.face_cascade = face_cascade
-
-        # Create a canvas that can fit the above video source size
-        self.canvas = tk.Canvas(
-            window,
-            width = 840,
-            height = 640
-            ) # 640x840
-        self.canvas.grid(row = 0, column = 0)
-
-        # Button snapshot
-        self.btn_onoff = tk.Button(
-            window,
-            text = "Turn on/off Camera",
-            width = 20,
-            height = 5,
-            command = self.toggle_camera
-            )
-        self.btn_onoff.grid(row = 1, column = 0)
-
-        # After it is called once, the update method will be automatically called every delay 15 milliseconds
-        self.delay = 15
+        button_play['state'] = 'disabled'
+        button_stop['state'] = 'normal'
         
-        self.update()
+        update_frame()
 
-        self.window.mainloop()
+def stop():
+    '''
+    stop stream (run_camera) 
+    and change state of buttons
+    '''
+    global run_camera
+
+    if run_camera:
+        run_camera = False
+
+        button_play['state'] = 'normal'
+        button_stop['state'] = 'disabled'
+
+def update_frame():
+
+    ret, frame = cap.read()
     
-    def toggle_camera(self):
-        # Toggle on/off Camera
-        self.camera_enable_flag == True
+    # correct the color
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        
-    def snapshot_face(self):
-        # Get a frame from the video source
-        ret, frame = self.vid.get_frame()
-        
-        if ret:
-            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg", cv2.cvtColor(frame, cv2.COLOR_RGB2BGR))
+    # mirror horizontally
+    frame = np.fliplr(frame)
+    img = Image.fromarray(frame)
+    photo_img.paste(img)
 
-    def update(self):
+    window_app.after(10, update_frame)
 
-        # open video source (by default this will try to open the computer webcam)
-        self.vid = LiveVideoCapture(self.window, self.video_source)
-            
-        # initialise face detection object
-        self.face_detect = FaceDetection(self.face_cascade)
 
-        # Get a frame from the video source
-        ret, frame = self.vid.get_frame()
+# --- main ---
 
-        # detect face in frame
-        face_coordinate = self.face_detect.detect_face(frame)
-        if len(face_coordinate) != 0:
-            (x, y, w ,h) = face_coordinate[0] # store only one set of face coordinate
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (0, 255, 0), 2)
+run_camera = False
 
-        if ret:
-            # swap horizontally
-            frame = np.fliplr(frame)
-            # put image 
-            self.photo = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
-            self.canvas.create_image(440, 340, image = self.photo)
+# Video Source Object
+cap = cv2.VideoCapture(1)
 
-        self.window.after(self.delay, self.update)
+# first frame
+ret, frame = cap.read()
+frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+frame = np.fliplr(frame)
+
+# create app window
+window_app = tk.Tk()
+window_app.geometry = ("880x800")
+window_app.resizable(width=True, height=True)
+
+image = Image.fromarray(frame)
+photo_img = ImageTk.PhotoImage(image)
 
 # declare face cascade model
 haar_face_cascade = cv2.CascadeClassifier('mdls/haarcascade_frontalface_default.xml')
 
-# Create a window and pass it to the Application object
-program_window = tk.Tk()
-program_window.geometry("880x800")
-program_window.resizable(width=True, height=True)
-main_program(program_window, "Face-Detector", face_cascade=haar_face_cascade)
+# Create a canvas that can fit the above video source size
+canvas = tk.Canvas(
+    window_app,
+    width = 840,
+    height = 640
+    ) # 640x840
+canvas.pack(fill='both', expand=True)
+
+canvas.create_image((0,0), image=photo_img, anchor='nw')
+
+# -- buttons
+buttons = tk.Frame(window_app)
+buttons.pack(fill='x')
+
+button_play = tk.Button(buttons, text="Play", command=play)
+button_play.pack(side='left')
+
+button_stop = tk.Button(buttons, text="Stop", command=stop, state='disabled')
+button_stop.pack(side='left')
+
+# -- /end buttons
+
+window_app.mainloop()
+
+cap.release()
