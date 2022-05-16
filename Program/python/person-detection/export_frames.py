b6bb21838828a94f1d3ca1c563c156159befaf50
paweenp <paween.pongsomboon@gmail.com>
2022-05-16 15:41:40 +0200

    remove all for tracking

--- Program/python/person-detection/export_frames.py
+++
@@ -1,9 +0,0 @@
-import cv2
-vidcap = cv2.VideoCapture('videos/Person_stand.mp4')
-success,image = vidcap.read()
-count = 0
-while success:
-  cv2.imwrite("images/frames/frame%d.jpg" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
